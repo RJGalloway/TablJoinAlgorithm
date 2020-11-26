0a5e5067bcb6d4c4c7af8c0950450413d869ecc2
Ryan Galloway <ryan.galloway@g.austincc.edu>
2020-11-26 13:33:04 -0600

    More techniques completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,18 +8,78 @@ public class Main
 {
     public static ArrayList<Integer> intersectionTechniqueOne(Integer m [], Integer n [])
     {
+        //Array list used to store intersected array so it will grow when common values are found
         ArrayList<Integer> intersectedArr = new ArrayList<Integer>();
 
+        //Java Arrays.sort uses Timsort: Average case is O(nlogn)
+        //Sort both arrays
+        Arrays.sort(m);
+        Arrays.sort(n);
+        
         return intersectedArr;
     }
     public static ArrayList<Integer> intersectionTechniqueTwo(Integer m [], Integer n [])
     {
         ArrayList<Integer> intersectedArr = new ArrayList<Integer>();
+        Arrays.sort(n);
+        int key;
+        int low;
+        int high;
+        for(int i = 0; i < m.length; i ++)
+        {
+            low = 0;
+            high = n.length - 1;
+            key = m[i];
+            while (low <= high)
+            {
+                int middle = (low + high) / 2 ;
+                if(n[middle] == key)
+                {
+                    intersectedArr.add(m[i]);
+                    break;
+                }
+                if(n[middle] < key)
+                {
+                    low = middle + 1;
+                }
+                if(n[middle] > key)
+                {
+                    high = middle -1;
+                }
+            }
+        }
         return intersectedArr;
     }
     public static ArrayList<Integer> intersectionTechniqueThree(Integer m [], Integer n [])
     {
         ArrayList<Integer> intersectedArr = new ArrayList<Integer>();
+        Arrays.sort(m);
+        int key;
+        int low;
+        int high;
+        for(int i = 0; i < n.length; i ++)
+        {
+            low = 0;
+            high = m.length - 1;
+            key = n[i];
+            while (low <= high)
+            {
+                int middle = (low + high) / 2 ;
+                if(m[middle] == key)
+                {
+                    intersectedArr.add(n[i]);
+                    break;
+                }
+                if(m[middle] < key)
+                {
+                    low = middle + 1;
+                }
+                if(m[middle] > key)
+                {
+                    high = middle -1;
+                }
+            }
+        }
         return intersectedArr;
     }
     public static ArrayList<Integer> intersectionTechniqueFour(Integer m [], Integer n [])
@@ -34,7 +95,6 @@ public class Main
                     break;
                 }
             }
-
         return intersectedArr;
     }
     public static void main(String[] args)
@@ -76,7 +136,9 @@ public class Main
 
         ArrayList<Integer> arrOne = intersectionTechniqueOne(arrM, arrN);
         ArrayList<Integer> arrTwo =intersectionTechniqueTwo(arrM, arrN);
+        System.out.println("Array using technique four" + arrTwo.toString());
         ArrayList<Integer> arrThree =intersectionTechniqueThree(arrM, arrN);
+        System.out.println("Array using technique four" + arrThree.toString());
         ArrayList<Integer> arrFour = intersectionTechniqueFour(arrM, arrN);
         System.out.println("Array using technique four" + arrFour.toString());
     }
