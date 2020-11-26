30df7b382fd3095e6eb57c9e89bd79c24f98a037
Ryan Galloway <ryan.galloway@g.austincc.edu>
2020-11-26 11:36:29 -0600

    Initial commit with technique four implemented

--- src/com/company/CreateArrays.java
+++ src/com/company/CreateArrays.java
@@ -6,17 +6,7 @@ import java.util.Collections;
 public class CreateArrays {
     Integer kArrValues [];
     Integer randArr [];
-/*    public CreateArrays(int k)
-    {
-        k = k;
-        Integer kArrValues [] = new Integer[k];
 
-        for (int i = 0; i < k; i++ )
-        {
-            kArrValues[i] = i;
-        }
-        Collections.shuffle(Arrays.asList(kArrValues));
-    }*/
     public Integer [] RandArrayCreator(int size, int k)
     {
         randArr = new Integer[size];
